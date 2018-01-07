c0de8eb55bf619d6ce776ee1a2672c9db0596d5b
pamputt <pamputt0@gmail.com>
2018-01-07 10:17:23 +0100

    Ajout de CreerCategories.py

---
+++ CreerCategories.py
@@ -0,0 +1,155 @@
+#! /usr/bin/python3
+
+# -*- coding: utf-8 -*-
+
+import csv
+import pywikibot
+
+import CleDeTri
+
+test = False # to test the script (without saving the result)
+
+def createCategoryPrononciationAudio(page,cle):
+  #Catégorie:Prononciations audio en biélorusse‏‎
+  beg=page.find(" en ")
+  language = page[beg+4:]
+  if (not language in cle):
+    return
+  
+  wikitext = "__HIDDENCAT__\n"
+  wikitext += "[[Catégorie:Prononciations audio|" + cle[language] + "]]"
+  return wikitext
+
+def createCategoryGrammaire(page,cle):
+  #Grammaire en abu’‏‎
+  beg=page.find(" en ")
+  language = page[beg+4:]
+  if (not language in cle):
+    return
+  
+  wikitext = "Cette catégorie a pour but de réunir les sous-catégories traitant de la grammaire en " + language + ", notamment :\n"
+  wikitext += "* Les natures de mots (nom, verbe…)\n"
+  wikitext += "* Les formes (locutions, sigles…)\n"
+  wikitext += "* les formes infléchies, déclinées, conjuguées…\n\n"
+  wikitext += "[[Catégorie:" + language + "|Grammaire]]\n"
+  wikitext += "[[Catégorie:Grammaire par langue|" + cle[language] +"]]"
+  return wikitext
+
+
+def createCategoryNomsCommuns(page,cle):
+  #Catégorie:Noms communs en saho‏‎
+  beg=page.find(" en ")
+  language = page[beg+4:]
+  if (not language in cle):
+    return
+  
+  wikitext = "[[Catégorie:Noms communs|" + cle[language] + "]]\n"
+  wikitext += "[[Catégorie:Grammaire en " + language + "]]"
+  return wikitext
+
+def createCategoryNomsPropresIssus(page,cle):
+  #Catégorie:Noms propres en français issus d’un mot en catalan
+  beg=page.find(" en ")
+  end=page.find(" issus")
+  language1=page[beg+4:end]
+  beg=page.find(" en ", end)
+  language2=page[beg+4:]
+  if (not language2 in cle):
+    return
+
+  wikitext = "[[Catégorie:Mots en " + language1 + " issus d’un mot en " + language2 + "]]\n"
+  wikitext += "[[Catégorie:Origines étymologiques des noms propres en " + language1 + "|" + cle[language2] + "]]"
+  return wikitext
+
+def createCategoryMotsIssus(page,cle):
+  #Mots en français issus d’un mot en catalan
+  beg=page.find(" en ")
+  end=page.find(" issus")
+  language1=page[beg+4:end]
+  beg=page.find(" en ", end)
+  language2=page[beg+4:]
+  if (language1 not in cle):
+    return
+
+  wikitext = "[[Catégorie:Origines étymologiques des mots en " + language1 + "|" + language2 + "]]"
+  wikitext += "[[Catégorie:Mots issus d’un mot en " + language2 + "|" + cle[language1] + "]]"
+  return wikitext
+
+def createCategory(page,cle):  
+  #Convert Category into string object
+  page = str(page)
+  beg=page.find(":")
+  end=page.find("]]")
+  page = page[beg+1:end]
+
+  wikitext = ""
+  if (page.find("Catégorie:Prononciations audio en") != -1):
+     wikitext = createCategoryPrononciationAudio(page,cle)
+  elif (page.find("Catégorie:Grammaire en") != -1):
+     wikitext = createCategoryGrammaire(page,cle)
+  elif (page.find("Catégorie:Noms communs en") != -1):
+     wikitext = createCategoryNomsCommuns(page,cle)
+  ## elif ((page.find("Catégorie:Noms propres en") != 1) and
+  ##       (page.find("issus d’un mot en") != -1)):
+  ##   createCategoryNomsPropresIssus(page,cle)
+  ## elif ((page.find("Catégorie:Mots en") != 1) and
+  ##       (page.find("issus d’un mot en") != -1)):
+  ##   createCategoryMotsIssus(page,cle)
+  else:
+    return
+
+  if not wikitext:
+    return
+  #TODO : corriger clé de tri
+  # en utilisant celle présente dans Module:langues/data
+  
+  pwbpage = pywikibot.Page(pywikibot.getSite(), page)
+  if not test:
+    pwbpage.put(wikitext)
+  else:
+    pywikibot.output(page + u' :\n' + wikitext)
+
+
+def getSortingKey():
+
+  cledetri = {}
+  with open("liste_langue.dat") as csvfile:
+    reader = csv.reader(csvfile, delimiter=';')
+    for row in reader:
+      name, code, key = row
+      cledetri[name] = key
+
+  return cledetri
+
+def WantedPagesCategoryGenerator(total=100, site=None):
+  """
+  Wanted category generator.
+
+  @param total: Maximum number of pages to retrieve in total
+  @type total: int
+  @param site: Site for generator results.
+  @type site: L{pywikibot.site.BaseSite}
+  """
+  
+  if site is None:
+    site = pywikibot.Site()
+  for page in site.wantedcategories(total=total):
+    yield page
+
+def main():
+  cle = getSortingKey()
+  
+  for page in WantedPagesCategoryGenerator(5000):
+    #print("Treating " + str(page))
+    createCategory(page,cle)
+  
+
+if __name__ == '__main__':
+
+  try:
+
+    main()
+    
+  finally:
+
+    pywikibot.stopme()
